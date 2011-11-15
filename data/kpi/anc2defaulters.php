fd6b7f992adb086235581d2c15640063e9a75870
Alex Little <alex@alexlittle.net>
2011-11-15 23:46:27 +0100

    new

---
+++ data/kpi/anc2defaulters.php
@@ -0,0 +1,30 @@
+<?php 
+
+/*
+* ANC2 defaulters
+*/
+$anc2_defaulters = array();
+// all those who had an ANC follow up visit
+$sql = "SELECT 	p._URI,
+				p.Q_USERID, 
+				p.Q_HEALTHPOINTID, 
+				p.Q_LMP, 
+				p._CREATION_DATE, 
+				DATE_ADD(p.Q_LMP, INTERVAL ".ANC2_DUE_BY_START." DAY) AS ANC2_DUE_BY_START,
+				DATE_ADD(p.Q_LMP, INTERVAL ".ANC2_DUE_BY_END." DAY) AS ANC2_DUE_BY_END
+		FROM ".ANCFOLLOW." p";
+echo $sql;
+echo "<br/>";
+// all those who had an ANC1 but not a second visit and didn't have termination protocol entered before ANC was due
+$sql = "SELECT 	p._URI,
+				p.Q_USERID, 
+				p.Q_HEALTHPOINTID, 
+				p.Q_LMP, 
+				p._CREATION_DATE, 
+				DATE_ADD(p.Q_LMP, INTERVAL ".ANC2_DUE_BY_START." DAY) AS ANC2_DUE_BY_START, 
+				DATE_ADD(p.Q_LMP, INTERVAL ".ANC2_DUE_BY_END." DAY) AS ANC2_DUE_BY_END 
+		FROM ".ANCFIRST." p
+		LEFT OUTER JOIN ".ANCFOLLOW." f ON f.Q_USERID = p.Q_USERID AND f.Q_HEALTHPOINTID = p.Q_HEALTHPOINTID
+		WHERE f.Q_USERID IS NULL";
+// @TODO add constraint about terminations
+echo $sql;
