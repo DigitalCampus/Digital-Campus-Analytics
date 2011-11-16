bd5e28794edf83256ff1d608dd83d2f852460ea9
Alex Little <alex@alexlittle.net>
2011-11-16 15:45:30 +0100

    summary data from ANC1 defaulters

--- includes/kpi/anc1defaulters.php
+++ includes/kpi/anc1defaulters.php
@@ -5,21 +5,45 @@
 /*
  * ANC1 defaulters
  */
-$anc1_defaulters = array();
+
+// get all the submitted ANC1 protocols from frist day of the month 6 months ago
 $sql = "SELECT 	p._URI, 
 				p.Q_USERID, 
 				p.Q_HEALTHPOINTID, 
 				p.Q_LMP, 
-				p._CREATION_DATE, 
+				p._CREATION_DATE as createdate, 
 				DATE_ADD(p.Q_LMP, INTERVAL ".ANC1_DUE_BY_END." DAY) AS ANC1DUEBY 
-		FROM ".ANCFIRST." p ";
+		FROM ".ANCFIRST." p 
+		WHERE p._CREATION_DATE > date_format(curdate() - interval 6 month,'%Y-%m-01 00:00:00')
+		ORDER BY p._CREATION_DATE ASC";
 
-// loop through
-// if creation date > ANC1DUEBY then defaulter, group by month/year of ANC1DUEBY 
+// set up summary/results array/objects
+$date = new DateTime();
+$date->sub(new DateInterval('P6M'));
+$summary = array();
+for ($i=0; $i<7 ;$i++){
+	$summary[$date->format('M-Y')] = new stdClass;
+	$summary[$date->format('M-Y')]->defaulter = 0;
+	$summary[$date->format('M-Y')]->nondefaulter = 0;
+	$date->add(new DateInterval('P1M'));
+}
+
+// exec query and loop through results
+// if creation date > ANC1DUEBY then defaulter, group by month/year of ANC1DUEBY
 // otherwise non defaulter
+$results = $API->runSql($sql);
+while($row = mysql_fetch_array($results)){
+	$date = new DateTime($row['createdate']);
+	$arrayIndex = $date->format('M-Y');
 
+	if ($row['createdate'] > $row['ANC1DUEBY'] ){
+		$summary[$arrayIndex]->defaulter++;
+	} else {
+		$summary[$arrayIndex]->nondefaulter++;
+	}
+}
 
-
+print_r($summary)
 ?>
 </div>
 
