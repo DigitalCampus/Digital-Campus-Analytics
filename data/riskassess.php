b4df79ca9ce36cc0a8633135310b5e2eeedeb052
Alex Little <alex@alexlittle.net>
2012-01-12 15:35:12 +0100

    add other risk factors

--- data/riskassess.php
+++ data/riskassess.php
@@ -91,29 +91,81 @@ class RiskAssessment {
 			$risk->count++;
 		}
 		
-		// Current pregnancy complaints
-		
-		
-		// Obstetrics/Gynaecology factors
-		
-		// Social factors
-		
-		// General medical factors
-		
-		//  
-		
-		
 		/*
-		 * eclampsia, bleeding, fatigue
+		 * Current pregnancy complaints
 		 */
-		/*$otherrisks = array ('Q_PREECLAMPSIA', 'Q_BLEEDING', 'Q_FATIGUE');
-		foreach ($otherrisks AS $r){
+		$risks = array ('Q_ABDOMINALPAIN', 'Q_BLEEDING', 'Q_FATIGUE','Q_FEVER','Q_HEADACHE');
+		foreach ($risks AS $r){
 			$risk->risks[$r] = false;
 			// from ANC First
 			if(isset($p->ancfirst->{$r}) && ($p->ancfirst->{$r} == 'yes')){
 				$risk->risks[$r] = true;
 			}
-			// from Transfer
+			// from ANC Follow
+			foreach($p->ancfollow as $x){
+				if(isset($x->{$r}) && ($x->{$r} == 'yes')){
+					$risk->risks[$r] = true;
+				}
+			}
+			if($risk->risks[$r] == true){
+				$risk->count++;
+			}
+		}
+		
+		/*
+		 * Obstetrics/Gynaecology factors
+		 */ 
+		$risks = array ('Q_STILLBIRTHS', 'Q_ABORTION');
+		foreach ($risks AS $r){
+			$risk->risks[$r] = false;
+			// from ANC First
+			if(isset($p->ancfirst->{$r}) && ($p->ancfirst->{$r} >= 1)){
+				$risk->risks[$r] = true;
+			}
+			// from ANC Transfer
+			foreach($p->anctransfer as $x){
+				if(isset($x->{$r}) && ($x->{$r} >= 1)){
+					$risk->risks[$r] = true;
+				}
+			}
+			if($risk->risks[$r] == true){
+				$risk->count++;
+			}
+		}
+		
+		$factor = 'Q_YOUNGESTCHILD';
+		$risk->risks[$factor] = false;
+		// from ANC First
+		if(isset($p->ancfirst->{$factor}) && ($p->ancfirst->{$factor} == 'no')){
+			$risk->risks[$factor] = true;
+		}
+		// from ANC Transfer
+		foreach($p->anctransfer as $x){
+			if(isset($x->{$factor}) && ($x->{$factor} == 'no')){
+				$risk->risks[$factor] = true;
+			}
+		}
+		if($risk->risks[$factor] == true){
+			$risk->count++;
+		}
+		
+		
+		$risks = array ('Q_PREECLAMPSIA', 
+						'Q_BLEEDINGPREVPREG',
+						'Q_CSECTION',
+						'Q_VACUUMDELIVERY',
+						'Q_NEWBORNDEATH',
+						'Q_PROLONGEDLABOR',
+						'Q_FISTULA',
+						'Q_MALPOSITION',
+						'Q_TWIN');
+		foreach ($risks AS $r){
+			$risk->risks[$r] = false;
+			// from ANC First
+			if(isset($p->ancfirst->{$r}) && ($p->ancfirst->{$r} == 'yes')){
+				$risk->risks[$r] = true;
+			}
+			// from ANC Transfer
 			foreach($p->anctransfer as $x){
 				if(isset($x->{$r}) && ($x->{$r} == 'yes')){
 					$risk->risks[$r] = true;
@@ -122,9 +174,267 @@ class RiskAssessment {
 			if($risk->risks[$r] == true){
 				$risk->count++;
 			}
-		}*/
+		}
 		
-
+		$factor = 'Q_BABYWEIGHT';
+		$risk->risks[$factor] = false;
+		// from ANC First
+		if(isset($p->ancfirst->{$factor}) && ($p->ancfirst->{$factor} != 'neither')){
+			$risk->risks[$factor] = true;
+		}
+		// from ANC Transfer
+		foreach($p->anctransfer as $x){
+			if(isset($x->{$factor}) && ($x->{$factor} != 'neither')){
+				$risk->risks[$factor] = true;
+			}
+		}
+		if($risk->risks[$factor] == true){
+			$risk->count++;
+		}
+		
+		$factor = 'Q_PREPOSTTERM';
+		$risk->risks[$factor] = false;
+		// from ANC First
+		if(isset($p->ancfirst->{$factor}) && ($p->ancfirst->{$factor} != 'neither')){
+			$risk->risks[$factor] = true;
+		}
+		// from ANC Transfer
+		foreach($p->anctransfer as $x){
+			if(isset($x->{$factor}) && ($x->{$factor} != 'neither')){
+				$risk->risks[$factor] = true;
+			}
+		}
+		if($risk->risks[$factor] == true){
+			$risk->count++;
+		}
+		
+		/*
+		 *  Social factors
+		 */ 
+		$risks = array ('Q_SOCIALSUPPORT', 'Q_ECONOMICS', 'Q_TRANSPORTATION');
+		foreach ($risks AS $r){
+			$risk->risks[$r] = false;
+			// from ANC First
+			if(isset($p->ancfirst->{$r}) && ($p->ancfirst->{$r} == 'no')){
+				$risk->risks[$r] = true;
+			}
+			// from ANC Transfer
+			foreach($p->anctransfer as $x){
+				if(isset($x->{$r}) && ($x->{$r} == 'no')){
+					$risk->risks[$r] = true;
+				}
+			}
+			if($risk->risks[$r] == true){
+				$risk->count++;
+			}
+		}
+		
+		/*
+		 * General medical factors
+		 */ 
+		$risks = array ('Q_DIABETES', 'Q_TUBERCULOSIS', 'Q_HYPERTENSION');
+		foreach ($risks AS $r){
+			$risk->risks[$r] = false;
+			// from ANC First
+			if(isset($p->ancfirst->{$r}) && ($p->ancfirst->{$r} == 'yes')){
+				$risk->risks[$r] = true;
+			}
+			// from ANC Follow
+			foreach($p->ancfollow as $x){
+				if(isset($x->{$r}) && ($x->{$r} == 'yes')){
+					$risk->risks[$r] = true;
+				}
+			}
+			if($risk->risks[$r] == true){
+				$risk->count++;
+			}
+		}
+		
+		$factor = 'Q_HIV';
+		$risk->risks[$factor] = false;
+		// from ANC First
+		if(isset($p->ancfirst->{$factor}) && ($p->ancfirst->{$factor} == 'positive')){
+			$risk->risks[$factor] = true;
+		}
+		// from ANC Follow
+		foreach($p->ancfollow as $x){
+			if(isset($x->{$factor}) && ($x->{$factor} == 'positive')){
+				$risk->risks[$factor] = true;
+			}
+		}
+		if($risk->risks[$factor] == true){
+			$risk->count++;
+		}
+		
+		/*
+		 * Physical exam factors
+		 */
+		$factor = 'Q_HEIGHT';
+		$risk->risks[$factor] = false;
+		// from ANC First
+		if(isset($p->ancfirst->{$factor}) && ($p->ancfirst->{$factor} == 'below150')){
+			$risk->risks[$factor] = true;
+		}
+		// from ANC Follow
+		foreach($p->ancfollow as $x){
+			if(isset($x->{$factor}) && ($x->{$factor} == 'below150')){
+				$risk->risks[$factor] = true;
+			}
+		}
+		if($risk->risks[$factor] == true){
+			$risk->count++;
+		}
+		
+		$factor = "Q_BLOODPRESSURE";
+		$risk->risks[$factor] = false;
+		// from ANC First
+		if(isset($p->ancfirst->Q_SYSTOLICBP) && isset($p->ancfirst->Q_DIASTOLICBP) 
+											&& (($p->ancfirst->Q_SYSTOLICBP >= 140) 
+												|| ($p->ancfirst->Q_SYSTOLICBP <= 90)
+												|| ($p->ancfirst->Q_DIASTOLICBP >= 90)
+												|| ($p->ancfirst->Q_DIASTOLICBP <= 60))){
+			$risk->risks[$factor] = true;
+		}
+		// from ANC Follow
+		foreach($p->ancfollow as $x){
+			if(isset($x->Q_SYSTOLICBP) && isset($x->Q_DIASTOLICBP)
+											&& (($x->Q_SYSTOLICBP >= 140) 
+											|| ($x->Q_SYSTOLICBP <= 90)
+											|| ($x->Q_DIASTOLICBP >= 90)
+											|| ($x->Q_DIASTOLICBP <= 60))){
+				$risk->risks[$factor] = true;
+			}
+		}
+		if($risk->risks[$factor] == true){
+			$risk->count++;
+		}
+		
+		$factor = 'Q_PALLORANEMIA';
+		$risk->risks[$factor] = false;
+		// from ANC First
+		if(isset($p->ancfirst->{$factor}) && ($p->ancfirst->{$factor} == 'pallor')){
+			$risk->risks[$factor] = true;
+		}
+		// from ANC Follow
+		foreach($p->ancfollow as $x){
+			if(isset($x->{$factor}) && ($x->{$factor} == 'pallor')){
+				$risk->risks[$factor] = true;
+			}
+		}
+		if($risk->risks[$factor] == true){
+			$risk->count++;
+		}
+		
+		$factor = 'Q_CARDIACPULSE';
+		$risk->risks[$factor] = false;
+		// from ANC First
+		if(isset($p->ancfirst->{$factor}) && (($p->ancfirst->{$factor} >= 100) || ($p->ancfirst->{$factor} <= 50))){
+			$risk->risks[$factor] = true;
+		}
+		// from ANC Follow
+		foreach($p->ancfollow as $x){
+			if(isset($x->{$factor}) && (($x->{$factor} >= 100) || ($x->{$factor} <= 50))){
+				$risk->risks[$factor] = true;
+			}
+		}
+		if($risk->risks[$factor] == true){
+			$risk->count++;
+		}
+		
+		$factor = 'Q_PRESENTATION';
+		$risk->risks[$factor] = false;
+		// from ANC First
+		if(isset($p->ancfirst->{$factor}) && (($p->ancfirst->{$factor} == 'breech') || ($p->ancfirst->{$factor} == 'transverse'))){
+			$risk->risks[$factor] = true;
+		}
+		// from ANC Follow
+		foreach($p->ancfollow as $x){
+			if(isset($x->{$factor}) && (($x->{$factor} == 'breech') || ($x->{$factor} == 'transverse'))){
+				$risk->risks[$factor] = true;
+			}
+		}
+		if($risk->risks[$factor] == true){
+			$risk->count++;
+		}
+		
+		$factor = 'Q_FETALHEARTRATEAUDIBLE';
+		$risk->risks[$factor] = false;
+		// from ANC First
+		if(isset($p->ancfirst->{$factor}) && ($p->ancfirst->{$factor} == 'notaudible')){
+			$risk->risks[$factor] = true;
+		}
+		// from ANC Follow
+		foreach($p->ancfollow as $x){
+			if(isset($x->{$factor}) && ($x->{$factor} == 'notaudible')){
+				$risk->risks[$factor] = true;
+			}
+		}
+		if($risk->risks[$factor] == true){
+			$risk->count++;
+		}
+		
+		/*
+		 * Test results
+		 */
+		$factor = 'Q_URINEPROTEIN';
+		$risk->risks[$factor] = false;
+		// from ANC Lab Test
+		foreach($p->anclabtest as $x){
+			/* TODO if(isset($x->{$factor}) && ($x->{$factor} == 'notaudible')){
+				$risk->risks[$factor] = true;
+			}*/
+		}
+		if($risk->risks[$factor] == true){
+			$risk->count++;
+		}
+		
+		$factor = 'Q_URINEGLUCOSE';
+		$risk->risks[$factor] = false;
+		// from ANC Lab Test
+		foreach($p->anclabtest as $x){
+			/* TODO if(isset($x->{$factor}) && ($x->{$factor} == 'notaudible')){
+			 $risk->risks[$factor] = true;
+			}*/
+		}
+		if($risk->risks[$factor] == true){
+			$risk->count++;
+		}
+		
+		$factor = 'Q_SYPHILIS';
+		$risk->risks[$factor] = false;
+		// from ANC Lab Test
+		foreach($p->anclabtest as $x){
+			if(isset($x->{$factor}) && ($x->{$factor} == 'positive')){
+			 $risk->risks[$factor] = true;
+			}
+		}
+		if($risk->risks[$factor] == true){
+			$risk->count++;
+		}
+		
+		$factor = 'Q_HEMOGLOBINLEVEL';
+		$risk->risks[$factor] = false;
+		// from ANC Lab Test
+		foreach($p->anclabtest as $x){
+			if(isset($x->{$factor}) && ($x->{$factor} < 12)){
+				$risk->risks[$factor] = true;
+			}
+		}
+		if($risk->risks[$factor] == true){
+			$risk->count++;
+		}
+		
+		$factor = 'Q_HEMATOCRITLEVEL';
+		$risk->risks[$factor] = false;
+		// from ANC Lab Test
+		foreach($p->anclabtest as $x){
+			if(isset($x->{$factor}) && ($x->{$factor} < 38)){
+				$risk->risks[$factor] = true;
+			}
+		}
+		if($risk->risks[$factor] == true){
+			$risk->count++;
+		}
 		
 		// work out the risk category
 		$risk->category = 'none';
