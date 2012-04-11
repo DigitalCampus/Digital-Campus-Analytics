06651a2aacb70d0dbda0c7e1d38db8b446751205
Alex Little <alex@alexlittle.net>
2012-04-11 16:00:03 +0200

    update anc for patient manager

--- includes/patient/anc.php
+++ includes/patient/anc.php
@@ -15,204 +15,339 @@
 <table class='rtable'>
 <tr class='rrow'>
 	<th><?php echo getstring('table.heading.question');?></th>
-	<th><?php echo getstring('table.heading.data');?></th>
+	<?php 
+		for($x=0;$x <count($anc); $x++ ){
+			echo "<th>".getstring('table.heading.data')."</th>";
+		}
+	?>
 </tr>
 <tr class="rrow">
 	<td class="rqcell"><?php echo getstring('protocolsubmitted');?></td>
 	<?php 
 		for($x=0;$x <count($anc); $x++ ){
 			echo "<td class='rdcell'>";
-				printf('%1$s %3$s (%2$s)<br/>%4$s (%5$s)',date('H:i',strtotime($ancfollow[$x]->CREATEDON)), 
-														date('D d M Y',strtotime($ancfollow[$x]->CREATEDON)), 
-														displayAsEthioDate(strtotime($ancfollow[$x]->CREATEDON)), 
-														$ancfollow[$x]->submittedname, 
-														displayHealthPointName($ancfollow[$x]->protocolhpcode));
+				printf('%1$s %3$s (%2$s)<br/>%4$s (%5$s)',date('H:i',strtotime($anc[$x]->CREATEDON)), 
+														date('D d M Y',strtotime($anc[$x]->CREATEDON)), 
+														displayAsEthioDate(strtotime($anc[$x]->CREATEDON)), 
+														$anc[$x]->submittedname, 
+														displayHealthPointName($anc[$x]->protocolhpcode));
 			echo "</td>";
 		}
 	?>
 </tr>
+
 <tr class="rrow">
 <td class="rqcell"><?php echo getstring('protocol.datevisitmade');?></td>
-	<td class="rdcell"><?php printf('%1$s (%2$s)',displayAsEthioDate(strtotime($patient->ancfirst->TODAY)), 
-																		date('D d M Y',strtotime($patient->ancfirst->TODAY)));?></td>
-</tr>
-<?php 
-	$rowArray = array(
-					'Q_USERID' => $patient->ancfirst->Q_USERID,
-					'Q_HEALTHPOINTID' => displayHealthPointName($patient->ancfirst->patienthpcode),
-					'Q_YEAROFBIRTH' => $patient->ancfirst->Q_YEAROFBIRTH,
-					'Q_AGE' => $patient->ancfirst->Q_AGE
-					);
-	foreach ($rowArray as $k=>$v){
-		generateANCFirstRow(getstring($k),$v);
-	}
-	
-?>
-<tr class='rrow'>
-	<td colspan="2" class="sh"><?php echo getstring('section.currentpregnancy');?></td>
-</tr>
-
-<?php 
-	$rowArray = array(
-					'Q_ABDOMINALPAIN' => $patient->ancfirst->Q_ABDOMINALPAIN,
-					'Q_BLEEDING' => $patient->ancfirst->Q_BLEEDING,
-					'Q_FATIGUE' => $patient->ancfirst->Q_FATIGUE,
-					'Q_FEVER' => $patient->ancfirst->Q_FEVER,
-					'Q_HEADACHE' => $patient->ancfirst->Q_HEADACHE,
-					'Q_OTHERHEALTHPROBLEMS' => $patient->ancfirst->Q_OTHERHEALTHPROBLEMS
-					);
-	foreach ($rowArray as $k=>$v){
-		generateANCFirstRow(getstring($k),$v);
-	}
-?>
-<tr class='rrow'>
-	<td colspan="2" class="sh"><?php echo getstring('section.previouspregnancy');?></td>
-</tr>
-<?php 
-
-	$q_babyweight = "";
-	if ($patient->ancfirst->Q_BABYWEIGHT != ""){
-		$q_babyweight = getstring("Q_BABYWEIGHT.".$patient->ancfirst->Q_BABYWEIGHT);
-	} 
-	
-	$temp = array();
-	foreach($patient->ancfirst->Q_FPMETHOD as $vv){
-		array_push($temp,getstring("Q_FPMETHOD.".strtolower($vv)));
-	}
-	$q_fpmethod = implode($temp,", ");
-	
-	$temp = array();
-	foreach($patient->ancfirst->Q_WHOATTENDED as $vv){
-		if(getstring("Q_WHOATTENDED.".$vv)){
-			array_push($temp,getstring("Q_WHOATTENDED.".$vv));
-		} else {
-			array_push($temp,$vv);
+	<?php 
+		for($x=0;$x <count($anc); $x++ ){
+			echo "<td class='rdcell'>";
+			if (isset($anc[$x])){
+				printf('%1$s (%2$s)',displayAsEthioDate(strtotime($anc[$x]->TODAY)), 
+																		date('D d M Y',strtotime($anc[$x]->TODAY)));
+			}
+			echo "</td>";
 		}
-	}
-	$q_whoattended = implode($temp,", ");
-	
-	$rowArray = array(
-					'Q_GRAVIDA' => $patient->ancfirst->Q_GRAVIDA,
-					'Q_PARITY' => $patient->ancfirst->Q_PARITY,
-					'Q_BIRTHINTERVAL' => $patient->ancfirst->Q_BIRTHINTERVAL,
-					'Q_LIVEBIRTHS' => $patient->ancfirst->Q_LIVEBIRTHS,
-					'Q_STILLBIRTHS' => $patient->ancfirst->Q_STILLBIRTHS,
-					'Q_YOUNGESTCHILD' => $patient->ancfirst->Q_YOUNGESTCHILD,
-					'Q_INFANTDEATH' => $patient->ancfirst->Q_INFANTDEATH,
-					'Q_ABORTION' => $patient->ancfirst->Q_ABORTION,
-					'Q_LIVINGCHILDREN' => $patient->ancfirst->Q_LIVINGCHILDREN,
-					'Q_PREECLAMPSIA' => $patient->ancfirst->Q_PREECLAMPSIA,
-					'Q_BLEEDINGPREVPREG' => $patient->ancfirst->Q_BLEEDINGPREVPREG,
-					'Q_CSECTION' => $patient->ancfirst->Q_CSECTION,
-					'Q_VACUUMDELIVERY' => $patient->ancfirst->Q_VACUUMDELIVERY,
-					'Q_NEWBORNDEATH' => $patient->ancfirst->Q_NEWBORNDEATH,
-					'Q_PROLONGEDLABOR' => $patient->ancfirst->Q_PROLONGEDLABOR,
-					'Q_FISTULA' => $patient->ancfirst->Q_FISTULA,
-					'Q_MALPOSITION' => $patient->ancfirst->Q_MALPOSITION,
-					'Q_TWIN' => $patient->ancfirst->Q_TWIN,
-					'Q_BABYWEIGHT' => $q_babyweight,
-					'Q_PREPOSTTERM' => $patient->ancfirst->Q_PREPOSTTERM,
-					'Q_OTHERPREVPREG' => $patient->ancfirst->Q_OTHERPREVPREG,
-					'Q_FAMILYPLAN' => $patient->ancfirst->Q_FAMILYPLAN,
-					'Q_FPMETHOD' => $q_fpmethod,
-					'Q_LMP' => displayAsEthioDate(strtotime($patient->ancfirst->Q_LMP))."<br/>".date('D d M Y',strtotime($patient->ancfirst->Q_LMP)),
-					'Q_EDD' => displayAsEthioDate(strtotime($patient->ancfirst->Q_EDD))."<br/>".date('D d M Y',strtotime($patient->ancfirst->Q_EDD)),
-					'Q_DELIVERYPLACE' => $patient->ancfirst->Q_DELIVERYPLACE,
-					'Q_WHOATTENDED' => $q_whoattended,
-					'Q_DELIVERYPLAN' => $patient->ancfirst->Q_DELIVERYPLAN,
-					'Q_SOCIALSUPPORT' => $patient->ancfirst->Q_SOCIALSUPPORT,
-					'Q_ECONOMICS' => $patient->ancfirst->Q_ECONOMICS,
-					'Q_TRANSPORTATION' => $patient->ancfirst->Q_TRANSPORTATION
-					);
-	foreach ($rowArray as $k=>$v){
-		generateANCFirstRow(getstring($k),$v);
-	}
-?>
-
-<tr class='rrow'>
-	<td colspan="2" class="sh"><?php echo getstring('section.medicalhistory');?></td>
+	?>
 </tr>
 
 <?php 
-	$rowArray = array(
-					'Q_DIABETES' => $patient->ancfirst->Q_DIABETES,
-					'Q_TUBERCULOSIS' => $patient->ancfirst->Q_TUBERCULOSIS,
-					'Q_HYPERTENSION' => $patient->ancfirst->Q_HYPERTENSION,
-					'Q_MALARIA' => $patient->ancfirst->Q_MALARIA,
-					'Q_BEDNETS' => $patient->ancfirst->Q_BEDNETS,
-					'Q_TETANUS' => getstring("Q_TETANUS.".$patient->ancfirst->Q_TETANUS),
-					'Q_TT1' => ($patient->ancfirst->Q_TT1 != "") ? displayAsEthioDate(strtotime($patient->ancfirst->Q_TT1))."<br/>".date('D d M Y',strtotime($patient->ancfirst->Q_TT1)) : "",
-					'Q_TT2' => ($patient->ancfirst->Q_TT2 != "") ? displayAsEthioDate(strtotime($patient->ancfirst->Q_TT2))."<br/>".date('D d M Y',strtotime($patient->ancfirst->Q_TT2)) : "",
-					'Q_TETANUS' => getstring("Q_TETANUS.".$patient->ancfirst->Q_TETANUS),
-					'Q_IRONTABLETS' => $patient->ancfirst->Q_IRONTABLETS,
-					'Q_IRONGIVEN' => $patient->ancfirst->Q_IRONGIVEN,
-					'Q_HIV' => $patient->ancfirst->Q_HIV,
-					'Q_HIVTREATMENT' => $patient->ancfirst->Q_HIVTREATMENT,
-					'Q_FOLICACID' => $patient->ancfirst->Q_FOLICACID,
-					'Q_MEBENDAZOL' => $patient->ancfirst->Q_MEBENDAZOL,
-					'Q_DRUGS' => $patient->ancfirst->Q_DRUGS,
-					'Q_DRUGSDESCRIPTION' => $patient->ancfirst->Q_DRUGSDESCRIPTION,
-					'Q_OTHERHEALTHPROBLEMS' => $patient->ancfirst->Q_OTHERHEALTHPROBLEMS
+	$rows = array(	'Q_USERID',	
+					'Q_HEALTHPOINTID',	
+					'Q_YEAROFBIRTH',
+					'Q_AGE' 
 					);
-	foreach ($rowArray as $k=>$v){
-		generateANCFirstRow(getstring($k),$v);
-	}
 	
-?>
-<tr class='rrow'>
-	<td colspan="2" class="sh"><?php echo getstring('section.examination');?></td>
-</tr>
-<?php 
-		
-	$q_presentation = "";
-	if ($patient->ancfirst->Q_PRESENTATION != ""){
-		$q_presentation = getstring("Q_PRESENTATION.".$patient->ancfirst->Q_PRESENTATION);
-	}
-	$q_fetalheartrateaudible = "";
-	if ($patient->ancfirst->Q_FETALHEARTRATEAUDIBLE != ""){
-		$q_fetalheartrateaudible = getstring("Q_FETALHEARTRATEAUDIBLE.".$patient->ancfirst->Q_FETALHEARTRATEAUDIBLE);
-	}
-	
-	$rowArray = array(
-					'Q_WEIGHT' => $patient->ancfirst->Q_WEIGHT,
-					'Q_HEIGHT' => $patient->ancfirst->Q_HEIGHT,
-					'Q_BLOODPRESSURE' => $patient->ancfirst->Q_SYSTOLICBP."/".$patient->ancfirst->Q_DIASTOLICBP,
-					'Q_PALLORANEMIA' => $patient->ancfirst->Q_PALLORANEMIA,
-					'Q_CARDIACPULSE' => $patient->ancfirst->Q_CARDIACPULSE,
-					'Q_EDEMA' => $patient->ancfirst->Q_EDEMA,
-					'Q_FUNDALHEIGHT' => $patient->ancfirst->Q_FUNDALHEIGHT,
-					'Q_GESTATIONALAGE' => $patient->ancfirst->Q_GESTATIONALAGE,
-					'Q_PRESENTATION' => $q_presentation,
-					'Q_FETALHEARTRATEAUDIBLE' => $q_fetalheartrateaudible,
-					'Q_FETALHEARTRATE24W' => $patient->ancfirst->Q_FETALHEARTRATE24W
-					);
-	foreach ($rowArray as $k=>$v){
-		generateANCFirstRow(getstring($k),$v);
-	}
+	foreach($rows as $r){
+		generateANCOpenRow(getstring($r));
+		$data = array();
+		for($x=0;$x <count($anc); $x++ ){
+			$data[$x] = array(
+							'Q_USERID' => $anc[$x]->Q_USERID,
+							'Q_HEALTHPOINTID'  => displayHealthPointName($anc[$x]->patienthpcode),
+							'Q_YEAROFBIRTH'  => $anc[$x]->Q_YEAROFBIRTH,
+							'Q_AGE'  => $anc[$x]->Q_AGE
+			);
+		}
+		for($x=0;$x <count($anc); $x++ ){
+			generateANCCell($data[$x][$r]);
+		}
+		generateANCCloseRow();
+	}	
 
-?>
+?>	
+
 
 <tr class='rrow'>
-	<td colspan="2" class="sh"><?php echo getstring('section.checklist');?></td>
+	<td colspan="<?php echo 1+count($anc);?>" class="sh"><?php echo getstring('section.currentpregnancy');?></td>
 </tr>
 
 <?php 
+	$rows = array(	'Q_ABDOMINALPAIN',
+					'Q_BLEEDING',
+					'Q_FATIGUE',
+					'Q_FEVER',
+					'Q_HEADACHE',
+					'Q_OTHERHEALTHPROBLEMS'
+					);
 	
-	$q_gpsdata = "";
-	if ($patient->ancfirst->Q_GPSDATA_LAT != ""){
-		$q_gpsdata = $patient->ancfirst->Q_GPSDATA_LAT.",".$patient->ancfirst->Q_GPSDATA_LNG;
-	}
-	$rowArray = array(
-					'Q_APPOINTMENTDATE' => displayAsEthioDate(strtotime($patient->ancfirst->Q_APPOINTMENTDATE))."<br/>".date('D d M Y',strtotime($patient->ancfirst->Q_APPOINTMENTDATE)),
-					'Q_IDCARD' => $patient->ancfirst->Q_IDCARD,
-					'Q_LOCATION' => getstring("Q_LOCATION.".$patient->ancfirst->Q_LOCATION),
-					'Q_GPSDATA' => $q_gpsdata,
-					
-	);
-	foreach ($rowArray as $k=>$v){
-		generateANCFirstRow(getstring($k),$v);
-	}
+	foreach($rows as $r){
+		generateANCOpenRow(getstring($r));
+		$data = array();
+		for($x=0;$x <count($anc); $x++ ){
+			$data[$x] = array(
+							'Q_ABDOMINALPAIN' => $anc[$x]->Q_ABDOMINALPAIN,
+							'Q_BLEEDING' => $anc[$x]->Q_BLEEDING,
+							'Q_FATIGUE' => $anc[$x]->Q_FATIGUE,
+							'Q_FEVER' => $anc[$x]->Q_FEVER,
+							'Q_HEADACHE' => $anc[$x]->Q_HEADACHE,
+							'Q_OTHERHEALTHPROBLEMS' => $anc[$x]->Q_OTHERHEALTHPROBLEMS
+							);
+		}
+		for($x=0;$x <count($anc); $x++ ){
+			generateANCCell($data[$x][$r]);
+		}
+		generateANCCloseRow();
+	}	
 
-?>
+?>	
+
+
+<tr class='rrow'>
+	<td colspan="<?php echo 1+count($anc);?>" class="sh"><?php echo getstring('section.previouspregnancy');?></td>
+</tr>
+
+<?php 
+	$rows = array(	
+					'Q_GRAVIDA',
+					'Q_PARITY',
+					'Q_BIRTHINTERVAL',
+					'Q_LIVEBIRTHS',
+					'Q_STILLBIRTHS',
+					'Q_YOUNGESTCHILD',
+					'Q_INFANTDEATH',
+					'Q_ABORTION',
+					'Q_LIVINGCHILDREN',
+					'Q_PREECLAMPSIA',
+					'Q_BLEEDINGPREVPREG',
+					'Q_CSECTION',
+					'Q_VACUUMDELIVERY',
+					'Q_NEWBORNDEATH',
+					'Q_PROLONGEDLABOR',
+					'Q_FISTULA',
+					'Q_MALPOSITION',
+					'Q_TWIN',
+					'Q_BABYWEIGHT',
+					'Q_PREPOSTTERM',
+					'Q_OTHERPREVPREG',
+					'Q_FAMILYPLAN',
+					'Q_FPMETHOD',
+					'Q_LMP',
+					'Q_EDD',
+					'Q_DELIVERYPLACE',
+					'Q_WHOATTENDED',
+					'Q_DELIVERYPLAN',
+					'Q_SOCIALSUPPORT',
+					'Q_ECONOMICS',
+					'Q_TRANSPORTATION'
+					);
+	
+	foreach($rows as $r){
+		generateANCOpenRow(getstring($r));
+		$data = array();
+		for($x=0;$x <count($anc); $x++ ){
+			$temp = array();
+			foreach($anc[$x]->Q_WHOATTENDED as $vv){
+				if(getstring("Q_WHOATTENDED.".$vv)){
+					array_push($temp,getstring("Q_WHOATTENDED.".$vv));
+				} else {
+					array_push($temp,$vv);
+				}
+			}
+			$q_whoattended = implode($temp,", ");
+			
+			$temp = array();
+			foreach($anc[$x]->Q_FPMETHOD as $vv){
+				if(getstring("Q_FPMETHOD.".$vv)){
+					array_push($temp,getstring("Q_FPMETHOD.".$vv));
+				} else {
+					array_push($temp,$vv);
+				}
+			}
+			$q_fpmethod = implode($temp,", ");
+			
+			$data[$x] = array(
+							'Q_GRAVIDA' => $anc[$x]->Q_GRAVIDA,
+							'Q_PARITY' => $anc[$x]->Q_PARITY,
+							'Q_BIRTHINTERVAL' => $anc[$x]->Q_BIRTHINTERVAL,
+							'Q_LIVEBIRTHS' => $anc[$x]->Q_LIVEBIRTHS,
+							'Q_STILLBIRTHS' => $anc[$x]->Q_STILLBIRTHS,
+							'Q_YOUNGESTCHILD' => $anc[$x]->Q_YOUNGESTCHILD,
+							'Q_INFANTDEATH' => $anc[$x]->Q_INFANTDEATH,
+							'Q_ABORTION' => $anc[$x]->Q_ABORTION,
+							'Q_LIVINGCHILDREN' => $anc[$x]->Q_LIVINGCHILDREN,
+							'Q_PREECLAMPSIA' => $anc[$x]->Q_PREECLAMPSIA,
+							'Q_BLEEDINGPREVPREG' => $anc[$x]->Q_BLEEDINGPREVPREG,
+							'Q_CSECTION' => $anc[$x]->Q_CSECTION,
+							'Q_VACUUMDELIVERY' => $anc[$x]->Q_VACUUMDELIVERY,
+							'Q_NEWBORNDEATH' => $anc[$x]->Q_NEWBORNDEATH,
+							'Q_PROLONGEDLABOR' => $anc[$x]->Q_PROLONGEDLABOR,
+							'Q_FISTULA' => $anc[$x]->Q_FISTULA,
+							'Q_MALPOSITION' => $anc[$x]->Q_MALPOSITION,
+							'Q_TWIN' => $anc[$x]->Q_TWIN,
+							'Q_BABYWEIGHT' => $anc[$x]->Q_BABYWEIGHT,
+							'Q_PREPOSTTERM' => $anc[$x]->Q_PREPOSTTERM,
+							'Q_OTHERPREVPREG' => $anc[$x]->Q_OTHERPREVPREG,
+							'Q_FAMILYPLAN' => $anc[$x]->Q_FAMILYPLAN,
+							'Q_FPMETHOD' => $q_fpmethod,
+							'Q_LMP' => displayAsEthioDate(strtotime($anc[$x]->Q_LMP))."<br/>".date('D d M Y',strtotime($anc[$x]->Q_LMP)),
+							'Q_EDD' => displayAsEthioDate(strtotime($anc[$x]->Q_EDD))."<br/>".date('D d M Y',strtotime($anc[$x]->Q_EDD)),
+							'Q_DELIVERYPLACE' => $anc[$x]->Q_DELIVERYPLACE,
+							'Q_WHOATTENDED' => $q_whoattended,
+							'Q_DELIVERYPLAN' => $anc[$x]->Q_DELIVERYPLAN,
+							'Q_SOCIALSUPPORT' => $anc[$x]->Q_SOCIALSUPPORT,
+							'Q_ECONOMICS' => $anc[$x]->Q_ECONOMICS,
+							'Q_TRANSPORTATION' => $anc[$x]->Q_TRANSPORTATION
+							);
+		}
+		for($x=0;$x <count($anc); $x++ ){
+			generateANCCell($data[$x][$r]);
+		}
+		generateANCCloseRow();
+	}	
+
+?>	
+
+
+<tr class='rrow'>
+	<td colspan="<?php echo 1+count($anc);?>" class="sh"><?php echo getstring('section.medicalhistory');?></td>
+</tr>
+
+<?php 
+	$rows = array(	
+					'Q_DIABETES',
+					'Q_TUBERCULOSIS',
+					'Q_HYPERTENSION',
+					'Q_MALARIA',
+					'Q_BEDNETS',
+					'Q_TETANUS',
+					'Q_TT1',
+					'Q_TT2',
+					'Q_TETANUS',
+					'Q_IRONTABLETS',
+					'Q_IRONGIVEN',
+					'Q_HIV',
+					'Q_HIVTREATMENT',
+					'Q_FOLICACID',
+					'Q_MEBENDAZOL',
+					'Q_DRUGS',
+					'Q_DRUGSDESCRIPTION',
+					'Q_OTHERHEALTHPROBLEMS'
+					);
+	
+	foreach($rows as $r){
+		generateANCOpenRow(getstring($r));
+		$data = array();
+		for($x=0;$x <count($anc); $x++ ){
+			$data[$x] = array(
+							'Q_DIABETES' => $anc[$x]->Q_DIABETES,
+							'Q_TUBERCULOSIS' => $anc[$x]->Q_TUBERCULOSIS,
+							'Q_HYPERTENSION' => $anc[$x]->Q_HYPERTENSION,
+							'Q_MALARIA' => $anc[$x]->Q_MALARIA,
+							'Q_BEDNETS' => $anc[$x]->Q_BEDNETS,
+							'Q_TETANUS' => getstring("Q_TETANUS.".$anc[$x]->Q_TETANUS),
+							'Q_TT1' => ($anc[$x]->Q_TT1 != "") ? displayAsEthioDate(strtotime($anc[$x]->Q_TT1))."<br/>".date('D d M Y',strtotime($anc[$x]->Q_TT1)) : "",
+							'Q_TT2' => ($anc[$x]->Q_TT2 != "") ? displayAsEthioDate(strtotime($anc[$x]->Q_TT2))."<br/>".date('D d M Y',strtotime($anc[$x]->Q_TT2)) : "",
+							'Q_TETANUS' => getstring("Q_TETANUS.".$anc[$x]->Q_TETANUS),
+							'Q_IRONTABLETS' => $anc[$x]->Q_IRONTABLETS,
+							'Q_IRONGIVEN' => $anc[$x]->Q_IRONGIVEN,
+							'Q_HIV' => $anc[$x]->Q_HIV,
+							'Q_HIVTREATMENT' => $anc[$x]->Q_HIVTREATMENT,
+							'Q_FOLICACID' => $anc[$x]->Q_FOLICACID,
+							'Q_MEBENDAZOL' => $anc[$x]->Q_MEBENDAZOL,
+							'Q_DRUGS' => $anc[$x]->Q_DRUGS,
+							'Q_DRUGSDESCRIPTION' => $anc[$x]->Q_DRUGSDESCRIPTION,
+							'Q_OTHERHEALTHPROBLEMS' => $anc[$x]->Q_OTHERHEALTHPROBLEMS
+							);
+		}
+		for($x=0;$x <count($anc); $x++ ){
+			generateANCCell($data[$x][$r]);
+		}
+		generateANCCloseRow();
+	}	
+?>	
+
+<tr class='rrow'>
+	<td colspan="<?php echo 1+count($anc);?>" class="sh"><?php echo getstring('section.examination');?></td>
+</tr>
+
+<?php 
+	$rows = array(	
+					'Q_WEIGHT',
+					'Q_HEIGHT',
+					'Q_BLOODPRESSURE',
+					'Q_PALLORANEMIA',
+					'Q_CARDIACPULSE',
+					'Q_EDEMA',
+					'Q_FUNDALHEIGHT',
+					'Q_GESTATIONALAGE',
+					'Q_PRESENTATION',
+					'Q_FETALHEARTRATEAUDIBLE',
+					'Q_FETALHEARTRATE24W'
+					);
+	
+	foreach($rows as $r){
+		generateANCOpenRow(getstring($r));
+		$data = array();
+		for($x=0;$x <count($anc); $x++ ){
+			$data[$x] = array(
+							'Q_WEIGHT' => $anc[$x]->Q_WEIGHT,
+							'Q_HEIGHT' => $anc[$x]->Q_HEIGHT,
+							'Q_BLOODPRESSURE' => $anc[$x]->Q_SYSTOLICBP."/".$anc[$x]->Q_DIASTOLICBP,
+							'Q_PALLORANEMIA' => $anc[$x]->Q_PALLORANEMIA,
+							'Q_CARDIACPULSE' => $anc[$x]->Q_CARDIACPULSE,
+							'Q_EDEMA' => $anc[$x]->Q_EDEMA,
+							'Q_FUNDALHEIGHT' => $anc[$x]->Q_FUNDALHEIGHT,
+							'Q_GESTATIONALAGE' => $anc[$x]->Q_GESTATIONALAGE,
+							'Q_PRESENTATION' => $anc[$x]->Q_PRESENTATION,
+							'Q_FETALHEARTRATEAUDIBLE' => $anc[$x]->Q_FETALHEARTRATEAUDIBLE,
+							'Q_FETALHEARTRATE24W' => $anc[$x]->Q_FETALHEARTRATE24W
+							);
+		}
+		for($x=0;$x <count($anc); $x++ ){
+			generateANCCell($data[$x][$r]);
+		}
+		generateANCCloseRow();
+	}	
+?>	
+
+<tr class='rrow'>
+	<td colspan="<?php echo 1+count($anc);?>" class="sh"><?php echo getstring('section.checklist');?></td>
+</tr>
+
+<?php 
+	$rows = array(	
+					'Q_APPOINTMENTDATE',
+					'Q_IDCARD',
+					'Q_LOCATION',
+					'Q_GPSDATA',
+					);
+	
+	foreach($rows as $r){
+		generateANCOpenRow(getstring($r));
+		$data = array();
+		for($x=0;$x <count($anc); $x++ ){
+			$data[$x] = array(
+							'Q_APPOINTMENTDATE' => displayAsEthioDate(strtotime($anc[$x]->Q_APPOINTMENTDATE))."<br/>".date('D d M Y',strtotime($anc[$x]->Q_APPOINTMENTDATE)),
+							'Q_IDCARD' => $anc[$x]->Q_IDCARD,
+							'Q_LOCATION' => getstring("Q_LOCATION.".$anc[$x]->Q_LOCATION),
+							'Q_GPSDATA' => ($anc[$x]->Q_GPSDATA_LAT != "") ? Q_GPSDATA_LAT."/".Q_GPSDATA_LNG :"",
+							);
+		}
+		for($x=0;$x <count($anc); $x++ ){
+			generateANCCell($data[$x][$r]);
+		}
+		generateANCCloseRow();
+	}	
+?>	
 
 </table>
