7aedca9e394da3435933a67e9ed79bc87d69ac0b
Alex Little <alex@alexlittle.net>
2012-05-17 14:33:34 +0200

    add migration script to cron task

--- admin/cron.php
+++ admin/cron.php
@@ -27,6 +27,15 @@ if(($lastrun + ($minint*60) > $now) && !$force){
 	die;
 }
 
+// run data migration script
+$f = fopen('migrate.sql',"r");
+$sqlFile = fread($f,filesize('migrate.sql'));
+$sqlArray = explode(';',$sqlFile);
+foreach ($sqlArray as $stmt) {
+	$result = mysql_query($stmt);
+	echo "ran sql\n";
+}
+
 $API->cron($flush, $days);
 
 echo "cron complete.";
